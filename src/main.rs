261df89ce4186b9dbd4b26b5b98a1ba3ee9093f5
liang <yeslaw90@hotmail.com>
2022-09-09 03:05:07 +0800

    fix: refractoring the code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,58 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
+
 
 fn main() {
+
+    // panic!("Problem");
+    // process::exit(101);
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1];
-    let filename: &String = &args[2];
+    let input: Input = Input::new(args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(0);
+    });
 
-    println!("Searching for {}", query);
-    println!("Filename {}", filename);
+    println!("Search Keyword: {}", input.query);
+    println!("Filename: {}", input.file_name);
 
-    let contents = fs::read_to_string(filename).expect("Something wrong with the file.");
+
+    if let Err(e) = run(input) {
+        eprintln!("Application error: {}", e);
+        process::exit(0);
+    }
+}
+
+
+fn run(input: Input) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(input.file_name)?;
     println!("With contents:\n {}", contents);
 
-    
-    
+    Ok(())
 }
+
+
+struct Input {
+    query: String,
+    file_name: String,
+}
+
+impl Input {
+    fn new(args: Vec<String>) -> Result<Input, String> {
+        if args.len() < 3 {
+            return Err(String::from("not enough arguments"));
+        }
+
+        let query: String = args[1].clone();
+        let file_name: String = args[2].clone();
+
+        Ok(Input { query, file_name })
+    }
+}
+
+
+
+
+
