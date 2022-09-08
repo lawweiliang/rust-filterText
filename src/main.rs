0aa4ab2fecd3840c62ca32403630a76a75073a18
liang <yeslaw90@hotmail.com>
2022-09-09 04:40:23 +0800

    feat: Extract code from main to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use rust_testing::{Input, run};
 
 
 fn main() {
-
-    // panic!("Problem");
-    // process::exit(101);
     let args: Vec<String> = env::args().collect();
 
     let input: Input = Input::new(args).unwrap_or_else(|err| {
@@ -18,7 +15,6 @@ fn main() {
     println!("Search Keyword: {}", input.query);
     println!("Filename: {}", input.file_name);
 
-
     if let Err(e) = run(input) {
         eprintln!("Application error: {}", e);
         process::exit(0);
@@ -26,31 +22,6 @@ fn main() {
 }
 
 
-fn run(input: Input) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(input.file_name)?;
-    println!("With contents:\n {}", contents);
-
-    Ok(())
-}
-
-
-struct Input {
-    query: String,
-    file_name: String,
-}
-
-impl Input {
-    fn new(args: Vec<String>) -> Result<Input, String> {
-        if args.len() < 3 {
-            return Err(String::from("not enough arguments"));
-        }
-
-        let query: String = args[1].clone();
-        let file_name: String = args[2].clone();
-
-        Ok(Input { query, file_name })
-    }
-}
 
 
 
